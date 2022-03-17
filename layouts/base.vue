47b585392edebffd5560393d381a0be57e664b4c
husniramdani <husniramdani18@gmail.com>
2022-03-17 15:56:53 +0700

    create header and footer etc

--- layouts/base.vue
+++ layouts/base.vue
@@ -1,8 +1,20 @@
 <template>
-  <div class="min-w-full overflow-hidden">
-    Header nanti disini
+  <div class="overflow-hidden">
+    <div
+      class="px-10 md:px-20 py-5 bg-blue-800 flex justify-between items-center"
+    >
+      <span class="flex items-center">
+        <img class="w-12" :src="require(`~/assets/logo.svg`)" alt="logo" />
+        <p class="text-white text-xl font-bold ml-3">SIObat</p>
+      </span>
+      <nuxt-link to="/" class="text-white font-semibold"> Beranda </nuxt-link>
+    </div>
     <Nuxt />
-    Footer disini
+    <div class="py-2 bg-blue-800 flex justify-center items-center">
+      <p class="text-white text-xl font-bold ml-3">
+        Made with &#10084;&#65039; from SV
+      </p>
+    </div>
   </div>
 </template>
 <script>
