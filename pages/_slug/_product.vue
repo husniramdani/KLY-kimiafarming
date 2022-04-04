2aecb61199ba0eee4944522c23bb8be032028b1d
husniramdani <husniramdani18@gmail.com>
2022-04-04 11:26:37 +0700

    update

---
+++ pages/_slug/_product.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="shadow rounded p-10 pt-2 m-16">
+    <input />
+    <h1 class="text-2xl font-bold">Product</h1>
+    <!-- <div class="mt-5 grid grid-cols-4 gap-4">
+      <div
+        v-for="(product, idx) in products"
+        :key="idx"
+      >
+        <button
+          class="block border text-left w-full p-5 rounded-md flex items-center hover:border-blue-500"
+        >
+          <img class="w-12 mr-3" :src="product.image_url" :alt="product.slug" />
+          <span> {{ product.name }} </span>
+        </button>
+      </div>
+    </div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "product",
+  layout: "base",
+  // async asyncData({ $axios, route }) {
+  //   const slug = route.query?.slug || "";
+  //   const products = await $axios
+  //     .$get(`/medicine/categories/${slug}/page/1`)
+  //     .then((res) => res.result);
+  //   return { products };
+  // },
+  methods: {
+    
+  }
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,20 @@
 <template>
-  <div class="shadow rounded p-10 m-16">
+  <div class="shadow rounded p-10 pt-2 m-16">
     <input />
-    <div v-for="(category, idx) in categories" :key="idx">
-      <button @click="onSearchCategory(category)">{{ category.slug }}</button>
+    <h1 class="text-2xl font-bold">Obat Berdasarkan Kategori</h1>
+    <div class="mt-5 grid grid-cols-4 gap-4">
+      <div
+        v-for="(category, idx) in categories"
+        :key="idx"
+      >
+        <button
+          @click="onSearchCategory(category)"
+          class="block border text-left w-full p-5 rounded-md flex items-center hover:border-blue-500"
+        >
+          <img class="w-12 mr-3" :src="category.image_url" :alt="category.slug" />
+          <span> {{ category.attributes.name }} </span>
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -19,13 +31,7 @@ export default {
   },
   methods: {
     onSearchCategory(category) {
-      this.$router.push({
-        name: "category",
-        path: "category",
-        params: {
-          category: category.slug,
-        },
-      });
+      this.$router.push(`${category.slug}`);
     },
   },
 };
